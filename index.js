a20fb43d0c3a3070331aecc35ec4ddcb77e2c723
Matheus <31835013+Blezef@users.noreply.github.com>
2020-06-12 19:27:46 -0300

    Organização e ajuste no código de soma

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
 var resultado = document.getElementById("resultado");
-var concatenacaoParaSoma = "";
-var concatenacaoParaTela = "";
+var concatenacao = "";
 
 function obtemNum(botao) {
   var valorBotao = document.getElementById(botao).innerHTML;
-  concatenacaoParaSoma += parseInt(valorBotao);
-  concatenacaoParaTela += valorBotao;
-  resultado.innerHTML = concatenacaoParaTela;
-  console.log(typeof concatenacaoParaSoma);
+  concatenacao += valorBotao;
+  resultado.innerHTML = concatenacao;
 }
 
 function mostrarResultado() {
-  var arraySoma = concatenacaoParaSoma.split(NaN);
-  console.log(concatenacaoParaSoma);
+  var arraySoma = concatenacao.split("+");
   var soma = 0;
 
   for (i = 0; i < arraySoma.length; i++) {
     soma += parseInt(arraySoma[i]);
   }
+
   return (resultado.innerHTML = soma);
 }
